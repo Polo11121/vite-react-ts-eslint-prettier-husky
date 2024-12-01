802118312b2ed925c4b6da1e6fb5a163df0230c6
Polo1121 <michal.jasinski11121@gmail.com>
2024-12-01 15:08:35 +0100

    add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,1 @@
-import { Test } from "@/features/test/Test";
-
-export const App = () => (
-  <div className="text-red-500">
-    vite-react-ts-eslint-prettier-husky
-    <Test />
-  </div>
-);
+export const App = () => <div className="text-red-500">vite-react-ts-eslint-prettier-husky</div>
