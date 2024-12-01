e125dcc472d1199fea7405e47d4e48c52842f938
Polo1121 <michal.jasinski11121@gmail.com>
2024-12-01 20:53:57 +0100

    Add react-router, react-query, axios, zustand and zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,10 @@
-export const App = () => <div className="text-red-500">vite-react-ts-eslint-prettier-husky</div>
+import { Route, Routes } from 'react-router'
+
+export const App = () => (
+  <Routes>
+    <Route
+      path="/"
+      element={<div className="text-red-500">vite-react-ts-eslint-prettier-husky</div>}
+    />
+  </Routes>
+)
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { App } from '@/App'
 import '@/index.css'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 )
