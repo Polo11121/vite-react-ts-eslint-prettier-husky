d1fcb53aa074cf730459f7b190f9d8a5134d609e
Polo1121 <michal.jasinski11121@gmail.com>
2024-12-01 16:06:33 +0100

    and vitest and react-testing-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.ts',
+    coverage: {
+      reporter: ['text', 'html']
+    },
+    exclude: [...configDefaults.exclude]
+  },
   resolve: {
     alias: {
       '@': '/src'
